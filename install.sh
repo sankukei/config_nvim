afea612558b252421cf33a96a242d623f8464954
sanku <sankukei@gmail.com>
2025-01-02 02:06:31 +0100

    improved script

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
 command_exist()
 {
 	command -v "$1" >/dev/null 2>&1
@@ -37,3 +43,8 @@ command git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
  command git clone https://github.com/sankukei/config_nvim.git\
  ~/.config/nvim
+
+command nvim --headless -c 'source ~/.config/nvim/lua/core/plugins.lua' -c 'PackerSync' -c 'autocmd User PackerComplete quitall'
+
+echo "All modules installed sucesfully"
+echo -e "${GREEN}Instalation completed :)${NC}" 
