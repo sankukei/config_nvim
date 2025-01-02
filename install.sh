fdf53530bc9bf671cdd6fd8bce49181f08fdf127
sanku <sankukei@gmail.com>
2025-01-02 01:37:39 +0100

    removed compiled packer

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ command_exist()
 	command -v "$1" >/dev/null 2>&1
 }
 
+echo "Deleting neovim and all config files"
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
+
 echo "Starting setup..."
 
 if command_exist nvim; then
@@ -26,3 +32,8 @@ fi
 
 command git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+ echo "Importing custom configuation..."
+
+ command git clone https://github.com/sankukei/config_nvim.git\
+ ~/.config/nvim
