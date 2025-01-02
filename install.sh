79c064eb835d12f05431e69dde7f4d8650eeea6e
sanku <sankukei@gmail.com>
2025-01-02 03:37:29 +0100

    add lock

--- install.sh
+++ install.sh
@@ -11,40 +11,58 @@ command_exist()
 	command -v "$1" >/dev/null 2>&1
 }
 
-echo "Deleting neovim and all config files"
+start_conf()
+{
+	echo "Deleting neovim and all config files"
 
-rm -rf ~/.config/nvim
-rm -rf ~/.local/share/nvim
-rm -rf ~/.cache/nvim
+	rm -rf ~/.config/nvim
+	rm -rf ~/.local/share/nvim
+	rm -rf ~/.cache/nvim
 
-echo "Starting setup..."
+	echo "Starting setup..."
 
-if command_exist nvim; then
-	echo "Nvim installed"
-else
-	echo "Installing Neovim..."
-
-	if command_exist apt; then
-		sudo apt update && sudo apt install -y neovim
-	elif command_exist brew; then
-		brew install neovim
-	elif command_exist pacman; then
-		sudo packman -S meovim --noconfirm
+	if command_exist nvim; then
+		echo "Nvim installed"
 	else
-        	echo "Could not detect a supported package manager. Please install Neovim manually."
+		echo "Installing Neovim..."
+
+		if command_exist apt; then
+			sudo apt update && sudo apt install -y neovim
+		elif command_exist brew; then
+			brew install neovim
+		elif command_exist pacman; then
+			sudo packman -S meovim --noconfirm
+		else
+        		echo "Could not detect a supported package manager. Please install Neovim manually."
 		exit 1
+		fi
 	fi
+
+	command git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+	 ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+	echo "Importing custom configuation..."
+
+	command git clone https://github.com/sankukei/config_nvim.git\
+	~/.config/nvim
+
+	command nvim --headless -c 'source ~/.config/nvim/lua/core/plugins.lua' -c 'PackerSync' -c 'autocmd User PackerComplete quitall'
+
+	echo "All modules installed sucesfully"
+	echo -e "${GREEN}Instalation completed :)${NC}" 
+}
+
+
+echo -e "${RED}This will delete all config files for neovim${NC}, please backup or abort if you care about these\n Do you want to continue ?"
+
+read -p "(yes/no) " yesno
+if [ "$yesno" == "yes" ] ; then
+	start_conf
+else
+{
+	echo "Aborting"	
+	exit 1
+}
 fi
 
-command git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
- echo "Importing custom configuation..."
-
- command git clone https://github.com/sankukei/config_nvim.git\
- ~/.config/nvim
-
-command nvim --headless -c 'source ~/.config/nvim/lua/core/plugins.lua' -c 'PackerSync' -c 'autocmd User PackerComplete quitall'
-
-echo "All modules installed sucesfully"
-echo -e "${GREEN}Instalation completed :)${NC}" 
